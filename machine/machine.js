11960f0754a2e862b129df627d1da42337298250
Rosinery <92558971+Rosinery@users.noreply.github.com>
2024-07-04 11:29:38 -0300

    Add files via upload

---
+++ machine/machine.js
@@ -0,0 +1,15 @@
+function clicmenu(){
+
+    let links = document.getElementById("links");
+
+    if(links.style.display== 'block'){
+        links.style.display='none'
+    } else{
+        links.style.display='block'
+    }
+}
+function MudouTamanho(){
+    if(window.innerWidth>=768){
+        links.style.display='block'
+    }else{links.style.display='none'}
+}
